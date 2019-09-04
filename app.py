8e598968c637edb4125d8870e3c68c919e72b99b
ludel <ludovic.delsol@ynov.com>
2019-09-04 22:14:04 +0200

    Check and download movies data

--- app.py
+++ app.py
@@ -1,58 +1,62 @@
 import os
 
 import pandas as pd
+import requests
 import tmdbsimple
-from bottle import run, get,abort
+from bottle import run, get, abort
 
 tmdbsimple.API_KEY = os.environ['TOKEN_KEY']
 DEBUG = os.environ.get('DEBUG', False)
 LANGUAGE = 'fr-FR'
 
 
-def movie():
-    instance = tmdbsimple.Movies
-
-    @get('/movies/<uid:int>')
-    def retrieve(uid):
-        return instance(uid).info(append_to_response='credits,keywords,videos,reviews')
-
-    @get('/movies/<uid:int>/recommendation')
-    def recommendation(uid):
-        df = pd.read_csv('data/movie.csv')
-        cluster = df.loc[df['id'] == uid].cluster
-        response = None
-
-        if cluster.any():
-            response = df.loc[df['cluster'] == int(cluster)].to_dict()
-
-        return response or abort(404, 'No result')
-
-    @get('/movies/top/<page:int>')
-    def top(page):
-        return instance().top_rated(page=page, language=LANGUAGE)
-
-    @get('/movies/popular/<page:int>')
-    def popular(page):
-        return instance().popular(page=page, language=LANGUAGE)
-
-    @get('/movies/now-playing/<page:int>')
-    def now_playing(page):
-        return instance().popular(page=page, language=LANGUAGE)
+@get('/movies/<uid:int>')
+def movie_retrieve(uid):
+    return tmdbsimple.Movies(uid).info(append_to_response='credits,keywords,videos,reviews')
 
 
-def people():
-    instance = tmdbsimple.People
+@get('/movies/<uid:int>/recommendation')
+def recommendation(uid):
+    df = pd.read_csv('data/movie.csv')
+    cluster = df.loc[df['id'] == uid].cluster
+    response = None
 
-    @get('/peoples/<uid:int>')
-    def retrieve(uid):
-        return instance(uid).info()
+    if cluster.any():
+        response = df.loc[df['cluster'] == int(cluster)].to_dict()
 
-    @get('/peoples/<uid:int>/movies')
-    def movies(uid):
-        return instance(uid).movie_credits(language=LANGUAGE)
+    return response or abort(404, 'No result')
+
+
+@get('/movies/top/<page:int>')
+def top(page):
+    return tmdbsimple.Movies().top_rated(page=page, language=LANGUAGE)
+
+
+@get('/movies/popular/<page:int>')
+def popular(page):
+    return tmdbsimple.Movies().popular(page=page, language=LANGUAGE)
+
+
+@get('/movies/now-playing/<page:int>')
+def now_playing(page):
+    return tmdbsimple.Movies().popular(page=page, language=LANGUAGE)
+
+
+@get('/peoples/<uid:int>')
+def people_retrieve(uid):
+    return tmdbsimple.People(uid).info()
+
+
+@get('/peoples/<uid:int>/movies')
+def people_movies(uid):
+    return tmdbsimple.People(uid).movie_credits(language=LANGUAGE)
 
 
 if __name__ == '__main__':
-    movie()
-    people()
+    if not os.path.exists('data/movie.csv'):
+        url = os.environ['DATA_URL']
+
+        with open('data/movie.csv', 'wb') as f:
+            f.write(requests.get(url).content)
+
     run(host='localhost', port=8080, debug=DEBUG)
