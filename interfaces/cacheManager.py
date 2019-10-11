8cf97db21c9e9c7f28e874be89ed6f9fe2701c3d
ludel <ludovic.delsol@ynov.com>
2019-10-11 20:42:20 +0200

    Manager 404 for id

--- interfaces/cacheManager.py
+++ interfaces/cacheManager.py
@@ -1,5 +1,8 @@
 import json
 
+from bottle import abort
+from requests import exceptions
+
 
 class CacheManager:
     def __init__(self, cache_db, key, expiration=86400):
@@ -11,9 +14,13 @@ class CacheManager:
         content = self.cache_db.get(self.key)
 
         if content:
-            content = json.loads(content)
-        else:
+            return json.loads(content)
+
+        try:
             content = request(**kwargs)
+        except exceptions.HTTPError:
+            return abort(404, 'Not found')
+        else:
             self.cache_db.set(self.key, json.dumps(content), ex=self.expiration)
 
         return content
